e02529d11edcb9555732e5a01b84eb246a7c576f
pranav sahu <pranavsahu688@gmail.com>
2026-06-01 01:38:01 +0530

    Added prediction pipeline for Random Forest model

---
+++ source/predict.py
@@ -0,0 +1,61 @@
+import joblib
+import pandas as pd
+
+model = joblib.load("models/fraud_detection_rf.pkl")
+scaler = joblib.load("models/scaler.pkl")
+
+# creating rediction function
+def predict_transaction(transaction_data):
+    """
+    transaction_data: dictionary containing transaction features
+    """
+
+    df = pd.DataFrame([transaction_data])
+
+    prediction = model.predict(df)[0]
+
+    probability = model.predict_proba(df)[0][1]
+
+    return {
+        "prediction": int(prediction),
+        "fraud_probability": round(float(probability), 4)
+    }
+
+# Test With Sample Transaction
+
+if __name__ == "__main__":
+
+    sample = {
+        "Time": 0,
+        "V1": 0,
+        "V2": 0,
+        "V3": 0,
+        "V4": 0,
+        "V5": 0,
+        "V6": 0,
+        "V7": 0,
+        "V8": 0,
+        "V9": 0,
+        "V10": 0,
+        "V11": 0,
+        "V12": 0,
+        "V13": 0,
+        "V14": 0,
+        "V15": 0,
+        "V16": 0,
+        "V17": 0,
+        "V18": 0,
+        "V19": 0,
+        "V20": 0,
+        "V21": 0,
+        "V22": 0,
+        "V23": 0,
+        "V24": 0,
+        "V25": 0,
+        "V26": 0,
+        "V27": 0,
+        "V28": 0,
+        "Amount": 100
+    }
+
+    print(predict_transaction(sample))
